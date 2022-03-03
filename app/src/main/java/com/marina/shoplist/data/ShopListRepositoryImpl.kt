db18ab64999c305379c4a4dc8e8d29855cefcd04
rivavl <mvkalinenkova@yandex.ru>
2022-03-04 02:40:26 +0300

    recycler view bug

--- app/src/main/java/com/marina/shoplist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/marina/shoplist/data/ShopListRepositoryImpl.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.marina.shoplist.domain.ShopItem
 import com.marina.shoplist.domain.ShopListRepository
+import kotlin.random.Random
 
 object ShopListRepositoryImpl : ShopListRepository {
 
@@ -12,8 +13,8 @@ object ShopListRepositoryImpl : ShopListRepository {
     private var autoIncrementedId = 0
 
     init {
-        for (i in 0 until 10) {
-            val item = ShopItem("Name $i", i, true)
+        for (i in 0 until 1000) {
+            val item = ShopItem("Name $i", i, Random.nextBoolean())
             addShopItem(item)
         }
     }
--- app/src/main/java/com/marina/shoplist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/marina/shoplist/presentation/ShopListAdapter.kt
@@ -4,13 +4,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.marina.shoplist.R
 import com.marina.shoplist.domain.ShopItem
 
 class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
 
-    val list = listOf<ShopItem>()
+    var shopList = listOf<ShopItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
     class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         val tvName = view.findViewById<TextView>(R.id.tv_name)
@@ -24,16 +29,43 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
     }
 
     override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
-        val shopItem = list[position]
+        val shopItem = shopList[position]
+        val status = if (shopItem.enabled) {
+            "Active"
+        } else {
+            "Not active"
+        }
 
-        viewHolder.tvName.text = shopItem.name
-        viewHolder.tvCount.text = shopItem.count.toString()
         viewHolder.view.setOnLongClickListener {
             true
         }
+
+        if (shopItem.enabled) {
+            viewHolder.tvName.text = "${shopItem.name} $status"
+            viewHolder.tvCount.text = shopItem.count.toString()
+            viewHolder.tvName.setTextColor(
+                ContextCompat.getColor(
+                    viewHolder.view.context,
+                    android.R.color.holo_red_light
+                )
+            )
+        }
+    }
+
+    //значения по умолчанию
+    override fun onViewRecycled(viewHolder: ShopItemViewHolder) {
+        super.onViewRecycled(viewHolder)
+        viewHolder.tvName.text = ""
+        viewHolder.tvCount.text = ""
+        viewHolder.tvName.setTextColor(
+            ContextCompat.getColor(
+                viewHolder.view.context,
+                android.R.color.white
+            )
+        )
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return shopList.size
     }
 }
