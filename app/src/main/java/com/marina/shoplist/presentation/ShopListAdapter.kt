4990b30b107c1e8ffede175b0415dfa3ae6cade7
rivavl <mvkalinenkova@yandex.ru>
2022-03-08 00:10:14 +0300

    add ShopListDiffCallback

--- app/src/main/java/com/marina/shoplist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/marina/shoplist/presentation/ShopListAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.marina.shoplist.R
 import com.marina.shoplist.domain.ShopItem
@@ -14,8 +15,10 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
     var count = 0
     var shopList = listOf<ShopItem>()
         set(value) {
+            val callback = ShopListDiffCallback(shopList, value)
+            val diffResult = DiffUtil.calculateDiff(callback)
+            diffResult.dispatchUpdatesTo(this)
             field = value
-            notifyDataSetChanged()
         }
 
     var onShopItemLongClickListener: ((ShopItem) -> Unit)? = null
