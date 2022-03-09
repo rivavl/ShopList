2bea4977edd006a0cfda12e859a08f192d5d2140
rivavl <mvkalinenkova@yandex.ru>
2022-03-10 02:30:39 +0300

    add ShopItemActivity with layout

---
+++ app/src/main/java/com/marina/shoplist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.marina.shoplist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.marina.shoplist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
