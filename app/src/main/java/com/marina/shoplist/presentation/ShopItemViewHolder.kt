5261ef1819adce48f236c63640a6030c182c359b
rivavl <mvkalinenkova@yandex.ru>
2022-04-10 14:49:49 +0300

    97. Использование Data Binding внутри адаптера RecyclerView

--- app/src/main/java/com/marina/shoplist/presentation/ShopItemViewHolder.kt
+++ app/src/main/java/com/marina/shoplist/presentation/ShopItemViewHolder.kt
@@ -1,11 +1,9 @@
 package com.marina.shoplist.presentation
 
-import android.view.View
-import android.widget.TextView
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
-import com.marina.shoplist.R
+import com.marina.shoplist.databinding.ItemShopDisabledBinding
 
-class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-    val tvName = view.findViewById<TextView>(R.id.tv_name)
-    val tvCount = view.findViewById<TextView>(R.id.tv_count)
-}
+class ShopItemViewHolder(
+    val binding: ViewDataBinding
+) : RecyclerView.ViewHolder(binding.root)
