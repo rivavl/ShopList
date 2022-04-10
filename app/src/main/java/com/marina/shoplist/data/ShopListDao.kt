2c6c46784c52d43d4e376a25269406c8dfc59e7e
rivavl <mvkalinenkova@yandex.ru>
2022-04-10 22:52:13 +0300

    111. Создание базы данных для приложения Shopping List

---
+++ app/src/main/java/com/marina/shoplist/data/ShopListDao.kt
@@ -0,0 +1,25 @@
+package com.marina.shoplist.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface ShopListDao {
+
+    @Query("SELECT * FROM shop_items")
+    fun getShopList(): LiveData<List<ShopItemDbModel>>
+
+    // если есть с таким же id, он перезапишется
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addShopItem(shopItemDbModel: ShopItemDbModel)
+
+    @Query("DELETE FROM shop_items WHERE id=:shopItemId")
+    fun deleteItem(shopItemId: Int)
+
+    // LIMIT на всякий случай
+    @Query("SELECT * FROM shop_items WHERE id=:shopItemId LIMIT 1")
+    fun getShopItem(shopItemId: Int): ShopItemDbModel
+}
